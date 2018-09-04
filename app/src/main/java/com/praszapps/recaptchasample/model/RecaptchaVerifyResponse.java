ba44941dbf0397fc30e3b61af8bea6fb7f6e3b0c
Prasan Pani <prasannajeet89@gmail.com>
2018-09-03 20:52:15 -0500

    Added error case to response

--- app/src/main/java/com/praszapps/recaptchasample/model/RecaptchaVerifyResponse.java
+++ app/src/main/java/com/praszapps/recaptchasample/model/RecaptchaVerifyResponse.java
@@ -1,10 +1,16 @@
 package com.praszapps.recaptchasample.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class RecaptchaVerifyResponse {
 
     private boolean success;
     private String challenge_ts;
     private String apk_package_name;
+    @SerializedName("error-codes")
+    private List<String> errorCodes;
 
     public boolean isSuccess() {
         return success;
@@ -13,8 +19,12 @@ public class RecaptchaVerifyResponse {
     public String getChallenge_ts() {
         return challenge_ts;
     }
+
     public String getApk_package_name() {
         return apk_package_name;
     }
 
+    public List<String> getErrorCodes() {
+        return errorCodes;
+    }
 }
